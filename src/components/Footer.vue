e9b5db76b99f4d9fea56de37f76f24d34ab93a35
mktcode <kontakt@markus-kottlaender.de>
2021-09-13 16:40:23 +0200

    changed footer button style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="bg-white">
     <div class="container mx-auto px-5 py-10 text-center space-x-3">
-      <a href="https://github.com/cryptoactions/cryptoactions.github.io/discussions" target="__blank" class="border-0 inline-block bg-indigo-600 text-gray-50 rounded-xl text-xl px-4 py-3">
+      <a href="https://github.com/cryptoactions/cryptoactions.github.io/discussions" target="__blank" class="inline-block text-indigo-600 hover:bg-indigo-600 hover:text-gray-50 rounded-xl text-xl px-4 py-3">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
         </svg>
         Forum
       </a>
-      <a href="https://github.com/cryptoactions" target="__blank" class="border-0 inline-block bg-indigo-600 text-gray-50 rounded-xl text-xl px-4 py-3">
+      <a href="https://github.com/cryptoactions" target="__blank" class="border-0 inline-block text-indigo-600 hover:bg-indigo-600 hover:text-gray-50 rounded-xl text-xl px-4 py-3">
         <i class="fab fa-github" />
       </a>
-      <a href="https://twitter.com/@cryptoactions" target="__blank" class="border-0 inline-block bg-indigo-600 text-gray-50 rounded-xl text-xl px-4 py-3">
+      <a href="https://twitter.com/@cryptoactions" target="__blank" class="border-0 inline-block text-indigo-600 hover:bg-indigo-600 hover:text-gray-50 rounded-xl text-xl px-4 py-3">
         <i class="fab fa-twitter" />
       </a>
     </div>
