0e881a47353e8b908fd77f9525e059d2eee6a78a
mktcode <kontakt@markus-kottlaender.de>
2021-09-13 03:45:22 +0200

    test

--- .github/workflows/count-contributions.js
+++ .github/workflows/count-contributions.js
@@ -1,8 +1,8 @@
 import { countContributions } from '@cryptoactions/sdk'
 
-const run = async ({ github, context, core }) => {
+const count = async ({ github, context, core }) => {
   const count = await countContributions(github.event.issue.user.login, github.token)
   core.setOutput('count', count)
 }
 
-export default run
+export default count
