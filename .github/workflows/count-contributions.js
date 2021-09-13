5c26231710b02e4190677f93321a2d38ea273d46
mktcode <kontakt@markus-kottlaender.de>
2021-09-14 01:57:12 +0200

    fixed workflow

--- .github/workflows/count-contributions.js
+++ .github/workflows/count-contributions.js
@@ -3,4 +3,5 @@ const { countContributions } = require('@cryptoactions/sdk')
 module.exports = async (context, core, githubToken) => {
   const count = await countContributions(context.payload.issue.user.login, githubToken)
   core.setOutput('count', count)
+  core.setOutput('tokens', count / 10)
 }
