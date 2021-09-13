b4040ad7b9d78071e08a3e3de026b1628367061a
mktcode <kontakt@markus-kottlaender.de>
2021-09-13 04:53:52 +0200

    debug

--- .github/workflows/count-contributions.js
+++ .github/workflows/count-contributions.js
@@ -1,7 +1,7 @@
 const { countContributions } = require('@cryptoactions/sdk')
 
 module.exports = async ({ github, context, core }) => {
-  console.log(github)
+  console.log(github.auth)
   const count = await countContributions(context.payload.issue.user.login, github.token)
   core.setOutput('count', count)
 }
